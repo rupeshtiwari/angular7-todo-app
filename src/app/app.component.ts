8370b88c5a9cd5dcafa2e72ae194ffa6d98a59fd
roopkt@gmail.com <roopkt@gmail.com>
2019-04-10 04:45:56 -0400

    added material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rt-todo';
+  todos = [];
+  create(task) {
+    this.todos.push({ id: Date.now, task });
+  }
 }
