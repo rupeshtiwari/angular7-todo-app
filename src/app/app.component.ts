f76f6a555d7cc1fe22aa6eca39ead074dab2905e
roopkt@gmail.com <roopkt@gmail.com>
2019-04-10 06:09:27 -0400

    add / delete todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,8 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  todos = [];
-  create(task) {
-    this.todos.push({ id: Date.now, task });
+  todoForm: FormGroup;
+  todoList = [];
+  id = 1;
+  constructor() {
+    this.todoForm = new FormGroup({
+      task: new FormControl('', [Validators.required, Validators.minLength(2)])
+    });
+  }
+
+  getErrorMessage() {
+    const taskForm = this.todoForm.controls.task;
+
+    return taskForm.hasError('required')
+      ? 'You must enter a task'
+      : taskForm.hasError('minlength')
+      ? 'Put more text'
+      : '';
+  }
+
+  create() {
+    this.todoList.push({ id: this.id++, task: this.task });
+  }
+
+  deleteTodo(id: number) {
+    this.todoList = this.todoList.filter(t => t.id !== id);
+  }
+
+  get task() {
+    return this.todoForm.get('task').value;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoMaterialModule } from './material-module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TodoMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
