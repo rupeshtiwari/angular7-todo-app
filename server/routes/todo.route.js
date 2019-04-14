43eb6d5ed1020d28e204e29e8306e48118e35d43
roopkt@gmail.com <roopkt@gmail.com>
2019-04-14 10:39:12 -0400

    fixing routes

--- server/routes/todo.route.js
+++ server/routes/todo.route.js
@@ -10,8 +10,7 @@ router.get('/all', asyncHandler(getAllTodos));
 async function insert(req, res, next) {
   console.log('todo from api', req.body);
   let todo = await todoCtrl.insert(req.body);
-  todo = todo.toObject();
-  res.json({ todo: todo });
+  res.json(todo);
 }
 
 async function getAllTodos(req, res, next) {
